cf1558b4cb836763c6eb709dcf67707507dfe5fa
unknown <gnzllosada>
2015-02-27 19:37:40 +0100

    Primer commint

---
+++ www/infotelefonoplugin.js
@@ -0,0 +1,12 @@
+var ejecutar = require('cordova/exec');
+
+var invocarNativo = function(exito, fracaso) {
+    ejecutar(exito, fracaso, 'infotelefonoplugin', 
+             'GET_IDENTIFICACION', []);
+};
+
+var plugin = {};
+plugin.obtenerInformacion = invocarNativo;
+
+module.exports = plugin;
+
